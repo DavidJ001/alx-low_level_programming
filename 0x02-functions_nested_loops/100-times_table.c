94ca611e14192a2e848a913cc79a3e015e2325d5
DavidJ001 <Davidjoel392@gmail.com>
2022-03-16 01:13:49 +0300

    tables failuire

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,49 @@
 #include "main.h"
 
 /**
- * print_times_table - Print the `n` times table, starting with 0.
- * Description: If `n` is greater than 15 or less than 0, print nothing.
- * @n: int type number
- */
-
+	* print_times_table - print the n times table, starting with 0.
+	* @n:the number .
+	* Return: (void) .
+**/
 void print_times_table(int n)
 {
-	int x = 0, y, z;
+int i, j, r;
 
-	if (n > 15 || n < 0)
-		return;
-	while (x <= n)
-	{
-		for (y = 0; y <= n; y++)
-		{
-			z = x * y;
-			if (z > 99)
-			{
-				_putchar(z / 100 + '0');
-				_putchar((z / 10 % 10) + '0');
-				_putchar(z % 10 + '0');
-			}
-			else if (z > 9)
-			{
-				_putchar(' ');
-				_putchar(z / 10 + '0');
-				_putchar(z % 10 + '0');
-			}
-			else if (y != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(z + '0');
-			}
-			else
-				_putchar(z + '0');
-
-			if (y != n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-		x++;
-	}
+if (((n <= 15) && (n >= 0)))
+{
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+r = i * j;
+if ((r > 9) && (r < 100))
+{
+_putchar((r / 10) + '0');
+_putchar((r % 10) + '0');
 }
+else if (r < 10)
+{
+_putchar(r + '0');
+}
+else
+{
+_putchar((r / 100) + '0');
+_putchar(((r / 10) % 10) + '0');
+_putchar((r % 10) + '0');
+}
+if (j < n)
+{
+_putchar(',');
+if ((i * (j + 1)) < 100 && (i * (j + 1)) >= 10)
+{
+_putchar(' ');
+_putchar(' ');
+}
+else if ((i * (j + 1)) >= 100)
+_putchar(' ');
+else
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');	}	}	}
+_putchar('\n');	}	}	}
