0c4ce12951c192f4a1d940506dffac9c8c002e45
DavidJ001 <Davidjoel392@gmail.com>
2022-05-03 21:19:32 +0300

    file io task  corrections

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,46 +1,42 @@
 #include "main.h"
 
 /**
- * _strlen - length of a string
- * @str: pointer to a string
+ * _strlen - finds the length of a string
+ * @str: pointer to the string
  *
- * Return: integer, str length
+ * Return: length of the string
  */
 size_t _strlen(char *str)
 {
-	size_t len;
+size_t i;
 
-	for (len = 0; str[len] != '\0'; len++)
-		;
-	return (len);
+for (i = 0; str[i]; i++)
+;
+return (i);
 }
 
 /**
- * create_file - creates a file if it doesn't exist
- * @filename: string, array for characters to name file
- * @text_content: string, contents to write into the file
+ * create_file - creates a file.
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
  *
- * Return: integer, 1 on success,
- * - -1 on failure | if filename == NULL | write fails
+ * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t bytes_written;
+int fd;
+ssize_t len = 0;
 
-	if (filename == NULL)
-		return (-1);
-
-	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return (-1);
-
-	if (text_content)
-	{
-		bytes_written = write(fd, text_content, _strlen(text_content));
-		close(fd);
-		if (bytes_written == -1)
-			return (-1);
-	}
-	return (1);
+if (filename == NULL)
+return (-1);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+return (-1);
+if (text_content != NULL)
+len = write(fd, text_content, _strlen(text_content));
+close(fd);
+if (len == -1)
+return (-1);
+return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,45 +1,41 @@
 #include "main.h"
 
 /**
- * _strlen - length of a string
- * @str: pointer to a string
+ * _strlen - finds the length of a string
+ * @str: pointer to the string
  *
- * Return: integer, str length
+ * Return: length of the string
  */
 size_t _strlen(char *str)
 {
-	size_t len;
+size_t i;
 
-	for (len = 0; str[len] != '\0'; len++)
-		;
-	return (len);
+for (i = 0; str[i]; i++)
+;
+return (i);
 }
 
 /**
- * append_text_to_file - append to a file
- * @filename: string, name of file
- * @text_content: string, array of characters to append
+ * append_text_to_file - appends a text at the end of a file.
+ * @filename: name of the file
+ * @text_content: NULL terminated string to add at the end of the file
  *
- * Return: 1 on success, -1 on failure | filename == NULL | write denied
+ * Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t bytes_written;
+int fd;
+ssize_t len;
 
-	if (filename == NULL)
-		return (-1);
-	if (text_content == NULL)
-		return (1);
-
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-		return (-1);
-
-	bytes_written = write(fd, text_content, _strlen(text_content));
-	close(fd);
-	if (bytes_written == -1)
-		return (-1);
-
-	return (1);
+if (filename == NULL)
+return (-1);
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd == -1)
+return (-1);
+if (text_content != NULL)
+len = write(fd, text_content, _strlen(text_content));
+close(fd);
+if (len == -1)
+return (-1);
+return (1);
 }
