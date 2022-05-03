7dd68a107e3ec9ec40f2495b6183b8c19d7651a8
DavidJ001 <Davidjoel392@gmail.com>
2022-05-03 21:51:45 +0300

    file io task  corrections

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,75 +1,76 @@
 #include "main.h"
-
-void closer(int arg_files);
-
 /**
- * main - Entry Point
- * @argc: # of args
- * @argv: array pointer for args
- * Return: 0
- */
-int main(int argc, char *argv[])
+  * condi_from - fills memory with a constant byte
+  * @file_from: is the size of the pointer
+  * Return: a int
+  */
+void condi_from(char *file_from)
 {
-int file_from, file_to, file_from_r, wr_err;
-char buf[1024];
-
-if (argc != 3)
-{
-dprintf(2, "Usage: cp file_from file_to\n");
-exit(97);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+	exit(98);
 }
-
-file_from = open(argv[1], O_RDONLY);
-if (file_from == -1)
-{
-dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
-
-file_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-if (file_to == -1)
-{
-dprintf(2, "Error: Can't write to %s\n", argv[2]);
-exit(99);
-}
-
-while (file_from_r >= 1024)
-{
-file_from_r = read(file_from, buf, 1024);
-if (file_from_r == -1)
-{
-dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-closer(file_from);
-closer(file_to);
-exit(98);
-}
-wr_err = write(file_to, buf, file_from_r);
-if (wr_err == -1)
-{
-dprintf(2, "Error: Can't write to %s\n", argv[2]);
-exit(99);
-}
-}
-
-closer(file_from);
-closer(file_to);
-return (0);
-}
-
 /**
- * closer - close with error
- * @arg_files: argv 1 or 2
- * Return: void
- */
-void closer(int arg_files)
+  * condi_to - fills memory with a constant byte
+  * @file_to: is the size of the pointer
+  * Return: a int
+  */
+void condi_to(char *file_to)
 {
-int close_err;
-
-close_err = close(arg_files);
-
-if (close_err == -1)
-{
-dprintf(2, "Error: Can't close fd %d\n", arg_files);
-exit(100);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+	exit(99);
 }
+/**
+  * condi_fd - fills memory with a constant byte
+  * @file: is the size of the pointer
+  * Return: a int
+  */
+void condi_fd(char *file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", file);
+	exit(100);
+}
+/**
+  * main - fills memory with a constant byte
+  * @argc: is the size of the pointer
+  * @argv: a index
+  * Return: a int
+  */
+
+int main(int argc, char **argv)
+{
+	int fd1 = 0, fd2 = 0, rd1 = 0, wr2 = 0;
+	char *file_from, *file_to, buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file_from = argv[1];
+	file_to = argv[2];
+
+	fd1 = open(file_from, O_RDONLY);
+	if (fd1 == -1)
+		condi_from(file_from);
+
+	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd2 == -1)
+		condi_to(file_to);
+
+	for (rd1 = read(fd1, buffer, 1024); rd1 > 0; rd1 = read(fd1, buffer, 1024))
+	{
+		wr2 = write(fd2, buffer, rd1);
+		if (wr2 == -1)
+			condi_to(file_to);
+	}
+
+	if (rd1 == -1)
+		condi_from(file_from);
+
+	if (close(fd1) == -1)
+		condi_fd(file_from);
+
+	if (close(fd2) == -1)
+		condi_fd(file_to);
+	return (0);
 }
